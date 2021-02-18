b233a50a0ea6d8ec0ef9dc388e4ed8458e00470b
91975 <91975@192.168.1.38>
2021-02-18 11:04:15 +0530

    made a little change

--- src/main/java/com/hotel/demo/model/Role.java
+++ src/main/java/com/hotel/demo/model/Role.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.sound.midi.VoiceStatus;
+
+import org.apache.tomcat.util.net.TLSClientHelloExtractor;
 
 @Entity
 public class Role {
@@ -41,4 +44,9 @@ public class Role {
 		return "Role [roleId=" + roleId + ", roleName=" + roleName + "]";
 	}
 	
+	public Void hello()
+	{
+		System.out.println("Hi");
+		return null;
+	}
 }
